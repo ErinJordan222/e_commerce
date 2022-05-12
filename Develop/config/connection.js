0885bab8714dec429fc40b8a6a41016b3aef7310
ErinJordan222 <erin.jordan@du.edu>
2022-05-12 11:12:02 -0600

    finished

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
+console.log(process.env)
+// if else, ternary
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  // : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    : new Sequelize('ecommerce_db', 'root', 'Fletch2711!', {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
